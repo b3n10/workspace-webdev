fb2770e689c052928ee15babb495a8f9894a84d7
b3n10 <francisben.lleve@gmail.com>
2018-06-08 23:06:34 +0800

    Add csso, uglify, autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-const gulp = require("gulp"),
-	babel = require("gulp-babel"),
-	browserSync = require("browser-sync").create();
+const gulp			= require("gulp"),
+	babel					= require("gulp-babel"),
+	browserSync 	= require("browser-sync").create(),
+	csso 					= require('gulp-csso'),
+	uglify				= require('gulp-uglify'),
+	autoprefixer	= require('gulp-autoprefixer');
+
+// supported browsers
+const AUTOPREFIXER_BROWSERS = [
+	'ie >= 10',
+	'ie_mob >= 10',
+	'ff >= 30',
+	'chrome >= 34',
+	'safari >= 7',
+	'opera >= 23',
+	'ios >= 7',
+	'android >= 4.4',
+	'bb >= 10'
+];
 
 gulp.task("default", () => {
 	return "No task selected.";
@@ -47,3 +63,33 @@ gulp.task("watch-babel", ["babel"], () => {
 	gulp.watch(["src/**/*.js"], ["babel"]);
 });
 
+// for php structure
+
+// transpile + minify
+gulp.task('php-babel', () => {
+	process.chdir(process.env.INIT_CWD);
+	gulp.src('./public_html/src/js/*.js')
+		.pipe(babel())
+		.on('error', (e) => {
+			console.log(`Error: ${e.name}\nMessage: ${e.message}\nLine: ${e.loc.line} Col: ${e.loc.column}`);// handle error for babel
+		})
+		.pipe(gulp.dest('./public_html/dist/js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./public_html/dist/js'));
+});
+
+// minify
+gulp.task('php-mincss', function () {
+	process.chdir(process.env.INIT_CWD);
+	return gulp.src('./public_html/src/css/style.css')
+		.pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+		.pipe(csso())
+		.pipe(gulp.dest('./public_html/dist/css/'));
+});
+
+
+gulp.task('watch-php', ['php-babel', 'php-mincss'], () => {
+	process.chdir(process.env.INIT_CWD);
+	gulp.watch(['**/src/*/*.js'], ['php-babel']);
+	gulp.watch(['**/src/*/*.css'], ['php-mincss']);
+});
